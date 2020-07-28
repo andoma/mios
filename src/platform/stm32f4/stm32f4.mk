8ff0d48eb7f5a1bf765bd8ebecc39e79c5b13628
Andreas Smas <andreas@lonelycoder.com>
2020-07-28 14:15:20 -0700

    stm32f4: Add DMA driver

--- src/platform/stm32f4/stm32f4.mk
+++ src/platform/stm32f4/stm32f4.mk
@@ -15,3 +15,4 @@ SRCS += ${P}/platform.c \
 	${P}/stm32f4_gpio.c \
 	${P}/stm32f4_i2c.c \
 	${P}/stm32f4_spi.c \
+	${P}/stm32f4_dma.c \
---
+++ src/platform/stm32f4/stm32f4_dma.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <error.h>
+
+typedef unsigned char stm32f4_dma_instance_t;
+
+typedef enum {
+  DMA_P_TO_M = 0,
+  DMA_M_TO_P = 1,
+} dma_direction_t;
+
+stm32f4_dma_instance_t stm32f4_dma_alloc_fixed(int controller, int stream);
+
+// IRQ_LEVEL_DMA must be masked
+void stm32f4_dma_start(stm32f4_dma_instance_t instance,
+                       void *maddr, uint32_t paddr, size_t items,
+                       dma_direction_t direction, int channel);
+
+// IRQ_LEVEL_DMA must be masked
+error_t stm32f4_dma_wait(stm32f4_dma_instance_t instance);
+
