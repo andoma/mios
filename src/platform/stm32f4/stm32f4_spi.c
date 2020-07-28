d41619ad198074483a49ad168057a1e491b08306
Andreas Smas <andreas@lonelycoder.com>
2020-07-28 14:15:46 -0700

    stm32f4/spi: Use DMA

--- src/platform/stm32f4/stm32f4_spi.c
+++ src/platform/stm32f4/stm32f4_spi.c
@@ -7,6 +7,7 @@
 #include "stm32f4.h"
 #include "stm32f4_clk.h"
 #include "stm32f4_spi.h"
+#include "stm32f4_dma.h"
 #include "mios.h"
 #include "task.h"
 
@@ -14,23 +15,40 @@
 
 struct stm32f4_spi {
   spi_t spi;
-
   uint32_t base_addr;
-
   mutex_t mutex;
-  struct task_queue waitable;
 
-  const uint8_t *tx;
-  uint8_t *rx;
-  size_t len;
-  size_t pos;
+  stm32f4_dma_instance_t rx_dma;
+  stm32f4_dma_instance_t tx_dma;
 };
-
 
 #define SPI_CR1  0x00
 #define SPI_CR2  0x04
 #define SPI_SR   0x08
 #define SPI_DR   0x0c
+
+static void
+spi_dma(struct stm32f4_spi *spi, const uint8_t *tx, uint8_t *rx, size_t len)
+{
+  stm32f4_dma_start(spi->tx_dma, (void *)tx, spi->base_addr + SPI_DR,
+                    len, DMA_M_TO_P, 0);
+
+  stm32f4_dma_start(spi->rx_dma, rx, spi->base_addr + SPI_DR,
+                    len, DMA_P_TO_M, 0);
+
+  reg_wr(spi->base_addr + SPI_CR2, 3);
+
+  error_t err;
+
+  err = stm32f4_dma_wait(spi->rx_dma);
+  assert(err == 0);
+  err = stm32f4_dma_wait(spi->tx_dma);
+  assert(err == 0);
+  reg_wr(spi->base_addr + SPI_CR2, 0);
+
+}
+
+
 
 static error_t
 spi_rw(spi_t *dev, const uint8_t *tx, uint8_t *rx, size_t len, gpio_t nss)
@@ -38,71 +56,30 @@ spi_rw(spi_t *dev, const uint8_t *tx, uint8_t *rx, size_t len, gpio_t nss)
   struct stm32f4_spi *spi = (struct stm32f4_spi *)dev;
   if(len == 0)
     return ERR_OK;
+
   mutex_lock(&spi->mutex);
   gpio_set_output(nss, 0);
-  spi->tx = tx;
-  spi->rx = rx;
-  spi->len = len;
-  spi->pos = 0;
-  int s = irq_forbid(IRQ_LEVEL_IO);
-  reg_wr(spi->base_addr + SPI_DR, tx[0]);
-  task_sleep(&spi->waitable, 0);
+
+  int s = irq_forbid(IRQ_LEVEL_DMA);
+  spi_dma(spi, tx, rx, len);
   irq_permit(s);
+
   gpio_set_output(nss, 1);
   mutex_unlock(&spi->mutex);
   return ERR_OK;
 }
 
 
-static void
-spi_irq(struct stm32f4_spi *spi)
-{
-  uint8_t b = reg_rd(spi->base_addr + SPI_DR);
-  if(spi->rx)
-    spi->rx[spi->pos] = b;
-  spi->pos++;
-  if(spi->pos == spi->len) {
-    task_wakeup(&spi->waitable, 0);
-    return;
-  }
-  reg_wr(spi->base_addr + SPI_DR, spi->tx[spi->pos]);
-}
-
-static struct stm32f4_spi *spis[3];
-
-void
-irq_35(void)
-{
-  spi_irq(spis[0]);
-}
-
-void
-irq_36(void)
-{
-  spi_irq(spis[1]);
-}
-
-void
-irq_51(void)
-{
-  spi_irq(spis[2]);
-}
-
 static const struct {
   uint16_t base;
   uint16_t clkid;
-  uint8_t irq;
-
-
+  uint8_t af;
 } spi_config[] = {
-  { 0x0130, CLK_SPI1, 35 },
-  { 0x0038, CLK_SPI2, 36 },
-  { 0x003c, CLK_SPI3, 51 },
+  { 0x0130, CLK_SPI1, 5 },
+  { 0x0038, CLK_SPI2, 5 },
+  { 0x003c, CLK_SPI3, 6 },
 };
 
-
-
-// PB13, PB14, PB15
 
 spi_t *
 stm32f4_spi_create(int instance, gpio_t clk, gpio_t miso,
@@ -118,23 +95,21 @@ stm32f4_spi_create(int instance, gpio_t clk, gpio_t miso,
   struct stm32f4_spi *spi = malloc(sizeof(struct stm32f4_spi));
   spi->base_addr = (spi_config[instance].base << 8) + 0x40000000;
   mutex_init(&spi->mutex);
-  TAILQ_INIT(&spi->waitable);
 
   reg_wr(spi->base_addr + SPI_CR1,
          (1 << 9) |
          (1 << 8) |
          (1 << 6) |
          (1 << 2) |
-         (7 << 3));
+         (2 << 3));
 
-  reg_wr(spi->base_addr + SPI_CR2,
-         (1 << 6));
+  assert(instance == 1); // Only DMA over SPI2 right now
 
-  spis[instance] = spi;
-  irq_enable(spi_config[instance].irq, IRQ_LEVEL_IO);
+  spi->rx_dma = stm32f4_dma_alloc_fixed(0, 3);
+  spi->tx_dma = stm32f4_dma_alloc_fixed(0, 4);
 
-  // Configure PB13...PB15 SPI
-  uint8_t af = 5;
+  // Configure GPIO
+  const uint8_t af = spi_config[instance].af;
   gpio_conf_af(clk,  af, GPIO_PUSH_PULL,  GPIO_SPEED_HIGH, GPIO_PULL_UP);
   gpio_conf_af(miso, af, GPIO_OPEN_DRAIN, GPIO_SPEED_HIGH, GPIO_PULL_NONE);
   gpio_conf_af(mosi, af, GPIO_PUSH_PULL,  GPIO_SPEED_HIGH, GPIO_PULL_NONE);
