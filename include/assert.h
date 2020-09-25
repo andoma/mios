b0d5fca4e0ad1f993aa72e9f5b12623574f489fb
Andreas Smas <andreas@lonelycoder.com>
2020-09-25 10:21:51 +0200

    assert.h: Correctly act on NDEBUG

--- include/assert.h
+++ include/assert.h
@@ -3,4 +3,8 @@
 void __assert_func(const char *expr, const char *file, int line)
   __attribute__((noreturn));
 
+#ifndef NDEBUG
 #define assert(EX) (void)((EX) || (__assert_func (#EX, __FILE__, __LINE__),0))
+#else
+#define assert(EX)
+#endif
