cc58280d0869964f9478ed78405c482a09d99224
Andreas Smas <andreas@lonelycoder.com>
2020-10-10 11:07:28 +0200

    Update sx1280

--- Makefile
+++ Makefile
@@ -46,7 +46,8 @@ SRCS += ${SRC}/drivers/ms5611.c
 SRCS += ${SRC}/drivers/mpu9250.c
 
 SRCS += ${SRC}/drivers/sx1280/sx1280.c \
-	${SRC}/drivers/sx1280/sx1280_mios.c
+	${SRC}/drivers/sx1280/sx1280_tdma.c \
+	${SRC}/drivers/sx1280_mios.c
 
 OBJS +=  ${SRCS:%.c=${O}/%.o}
 OBJS :=  ${OBJS:%.s=${O}/%.o}
---
+++ src/drivers/sx1280_pal.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <error.h>
+#include <io.h>
+#include <task.h>
+#include <malloc.h>
+
+typedef mutex_t sx1280_mutex_t;
+typedef cond_t sx1280_cond_t;
+typedef task_t *sx1280_thread_t;
+
+#define sx1280_malloc(s) xalloc(s, 0, MEM_TYPE_DMA)
+#define sx1280_free(p) free(p)
+
+#define sx1280_mutex_lock(m)   mutex_lock(m)
+#define sx1280_mutex_unlock(m) mutex_unlock(m)
+#define sx1280_cond_wait(c, m) cond_wait(c, m)
+#define sx1280_cond_signal(c)  cond_signal(c)
+#define sx1280_clock_get() clock_get()
+#define sx1280_thread_create(t, p, a, pri) *(t) = task_create(p, a, 1024, "sx1280", TASK_DMA_STACK, pri)
+
+
+
+typedef error_t sx1280_err_t;
+
+#define SX1280_ERR_INVALID ERR_INVALID_ID
+#define SX1280_ERR_MTU     ERR_MTU_EXCEEDED
+#define SX1280_ERR_NO_BUFS ERR_NO_BUFFER
+
+sx1280_t *sx1280_create(spi_t *bus, gpio_t nss, gpio_t busy,
+                        gpio_t irq, gpio_t reset,
+                        gpio_t dbg1, gpio_t dbg2);
+
