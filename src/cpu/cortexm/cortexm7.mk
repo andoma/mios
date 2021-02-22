9a50642a5524767c30973a3c283246ae8f1bbc66
Andreas Smas <andreas@lonelycoder.com>
2021-02-22 08:17:40 +0100

    cortexm7: Set -mfpu=fpv5-d16

--- src/cpu/cortexm/cortexm7.mk
+++ src/cpu/cortexm/cortexm7.mk
@@ -1,6 +1,6 @@
 GLOBALDEPS += ${SRC}/cpu/cortexm/cortexm7.mk
 
-CFLAGS += -mcpu=cortex-m7 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard
+CFLAGS += -mcpu=cortex-m7 -mthumb -mfloat-abi=hard -mfpu=fpv5-d16
 
 CPPFLAGS += -include ${SRC}/cpu/cortexm/cortexm7.h
 
