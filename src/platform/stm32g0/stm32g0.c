ef45e0c1f98860a9df8ec28fe1e6eeb770552859
Andreas Smas <andreas@lonelycoder.com>
2021-02-25 23:12:06 +0100

    stm32g0: Set correct RAM size based on chipid

--- src/platform/stm32g0/stm32g0.c
+++ src/platform/stm32g0/stm32g0.c
@@ -1,24 +1,49 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <malloc.h>
 
+
 // Perhaps use 0x40015800 DBG_IDCODE
+
+static volatile uint16_t *const FLASH_SIZE = (volatile uint16_t *)0x1fff75e0;
+static volatile uint32_t *const DBG_IDCODE = (volatile uint32_t *)0x40015800;
+
+static struct {
+  uint16_t id;
+  uint16_t ramsize;
+  const char name[4];
+} devicemap[] = {
+  { 0x466, 8, "3x" },
+  { 0x460, 32, "7x" },
+};
+
 
 static void  __attribute__((constructor(120)))
 stm32g0_init(void)
 {
   extern unsigned long _ebss;
 
-  void *SRAM1_start = (void *)&_ebss;
-  void *SRAM1_end   = (void *)0x20000000 + 36 * 1024;
+  uint32_t chipid = *DBG_IDCODE;
+  uint16_t deviceid = chipid & 0xfff;
 
-  //  printf("\nSTM32F4 (%d kB Flash)\n", *FLASH_SIZE);
+  int ramsize = 8;
+  const char *name = "???";
+  for(size_t i = 0; i < sizeof(devicemap) / sizeof(devicemap[0]); i++) {
+    if(devicemap[i].id == deviceid) {
+      ramsize = devicemap[i].ramsize;
+      name = devicemap[i].name;
+      break;
+    }
+  }
+
+  printf("\nSTM32G0%s (%d kB Flash) (%d kB RAM) ID:0x%08x\n",
+         name, *FLASH_SIZE, ramsize, chipid);
+
+  void *SRAM1_start = (void *)&_ebss;
+  void *SRAM1_end   = (void *)0x20000000 + ramsize * 1024;
 
   // SRAM1
   heap_add_mem((long)SRAM1_start, (long)SRAM1_end, MEM_TYPE_DMA);
-#if 0
-   // CCM
-  clk_enable(CLK_CCMDATARAMEN);
-  heap_add_mem(0x10000000, 0x10010000, MEM_TYPE_LOCAL);
-#endif
+
 }
 
