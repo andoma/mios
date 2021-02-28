18099683aaf3ccf4b1380cb4f0b4c87c06411389
Andreas Smas <andreas@lonelycoder.com>
2021-02-28 15:06:25 +0100

    shell: Add i2c-write

--- src/shell/cmd_i2c.c
+++ src/shell/cmd_i2c.c
@@ -75,3 +75,42 @@ cmd_i2c_read(cli_t *cli, int argc, char **argv)
 
 
 CLI_CMD_DEF("i2c-read", cmd_i2c_read);
+
+
+static int
+cmd_i2c_write(cli_t *cli, int argc, char **argv)
+{
+  if(argc < 5) {
+    cli_printf(cli, "i2c-read <bus> <addr> <reg> <value> ... \n");
+    return -1;
+  }
+
+  const int bus_id = atoi(argv[1]);
+  const int addr = atoix(argv[2]);
+  const uint8_t reg = atoix(argv[3]);
+  i2c_t *bus = i2c_get_bus(bus_id);
+  if(bus == NULL) {
+    cli_printf(cli, "i2c bus %d not available\n", bus_id);
+    return 0;
+  }
+
+  argc -= 4;
+  argv += 4;
+
+  uint8_t *buf = malloc(1 + argc);
+
+  buf[0] = reg;
+  for(int i = 0; i < argc; i++)
+    buf[1 + i] = atoix(argv[i]);
+
+  error_t err = bus->rw(bus, addr, buf, argc + 1, NULL, 0);
+
+  if(err) {
+    cli_printf(cli, "ERR: %d\n", err);
+  }
+  free(buf);
+  return 0;
+}
+
+
+CLI_CMD_DEF("i2c-write", cmd_i2c_write);
