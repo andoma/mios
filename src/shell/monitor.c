e1794b2fccaaa799be9bda718d34a6485e048fab
Andreas Smas <andreas@lonelycoder.com>
2021-04-07 20:22:50 +0200

    shell: Add rd32 command

--- src/shell/monitor.c
+++ src/shell/monitor.c
@@ -46,3 +46,30 @@ cmd_md(cli_t *cli, int argc, char **argv)
 
 
 CLI_CMD_DEF("md", cmd_md);
+
+
+
+
+static int
+cmd_rd32(cli_t *cli, int argc, char **argv)
+{
+  if(argc < 3) {
+    cli_printf(cli, "rd32 <start> <count>\n");
+    return -1;
+  }
+
+  const int start = atoix(argv[1]);
+  const int count = atoix(argv[2]);
+
+  uint32_t *ptr = (uint32_t *)start;
+  for(int i = 0; i < count; i++) {
+    cli_printf(cli, "0x%04x: 0x%08x\n",
+           4 * i, *ptr);
+    ptr++;
+  }
+  return 0;
+}
+
+
+
+CLI_CMD_DEF("rd32", cmd_rd32);
