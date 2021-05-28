7255518afec70c35a146f2fef712b7546e1974b3
Andreas Smas <andreas@lonelycoder.com>
2021-05-28 21:28:08 +0200

    stm32f4: Get rid of STM32F4_APBxCLOCK -defines

--- src/platform/stm32f4/stm32f4_clk.c
+++ src/platform/stm32f4/stm32f4_clk.c
@@ -10,17 +10,27 @@
 #define RCC_CFGR    0x40023808
 
 
+static uint32_t apb1clock;
+static uint32_t apb2clock;
+
+
 int
 clk_get_freq(uint16_t id)
 {
+  uint32_t r;
   switch(id >> 8) {
   default:
     panic("clk_get_speed() invalid id: 0x%x", id);
   case CLK_APB1:
-    return STM32F4_APB1CLOCK;
+    r = apb1clock;
+    break;
   case CLK_APB2:
-    return STM32F4_APB2CLOCK;
+    r = apb2clock;
+    break;
   }
+  if(!r)
+    panic("Clock 0x%x not initialized", id);
+  return r;
 }
 
 
@@ -57,6 +67,9 @@ stm32f4_init_pll(int hse_freq)
   uint32_t pllcfgr = 0;
   reg_wr(FLASH_ACR, 0x705); // D-CACHE I-CACHE PREFETCH, 5 wait states
 
+  apb1clock = CPU_SYSTICK_RVR / 4;
+  apb2clock = CPU_SYSTICK_RVR / 2;
+
   reg_wr(RCC_CFGR,
          (0x7 << 27) | // MCO2PRE /5
          (0x4 << 13) | // APB2 (High speed) prescaler = 2
@@ -67,15 +80,17 @@ stm32f4_init_pll(int hse_freq)
     while(!(reg_rd(RCC_CR) & (1 << 17))) {} // Wait for external oscillator
     pll_m = hse_freq / 2;
     pllcfgr |= (1 << 22);
-
   }
+
+  // Clock for USB should be 48MHz
+  const uint32_t pll_q = CPU_SYSCLK_MHZ * 2 / 48;
 
   reg_wr(RCC_PLLCFGR,
          pllcfgr |
          (pll_m << 0) |          // input division
          (CPU_SYSCLK_MHZ << 6) | // PLL multiplication
          (0 << 16) |             // PLL sys clock division (0 == /2)
-         (7 << 24));             // PLL usb clock division =48MHz
+         (pll_q << 24));         // PLL usb clock division
 
   reg_set_bit(RCC_CR, 24);
 
