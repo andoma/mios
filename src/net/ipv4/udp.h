6f702c102386fd43f18cb0d24bfd48ca0d69f9da
Andreas Smas <andreas@lonelycoder.com>
2021-08-11 08:40:12 +0200

    net/udp: Remove unused function declarations

--- src/net/ipv4/udp.h
+++ src/net/ipv4/udp.h
@@ -1,14 +1,8 @@
 #pragma once
 
-#include <mios/error.h>
-
 struct netif;
 struct pbuf;
 struct socket;
 
 struct pbuf *udp_input_ipv4(struct netif *ni, struct pbuf *pb,
                             int udp_offset);
-
-error_t udp_socket_attach(struct socket *s);
-
-error_t udp_socket_detach(struct socket *s);
