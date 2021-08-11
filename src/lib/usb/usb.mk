47917096a293921b957d870ebf1e665eb562b827
Andreas Smas <andreas@lonelycoder.com>
2021-08-11 08:21:30 +0200

    Only build lib/usb/usb_mbus.c if ENABLE_NET_MBUS

--- src/lib/usb/usb.mk
+++ src/lib/usb/usb.mk
@@ -2,6 +2,8 @@ GLOBALDEPS += ${SRC}/lib/usb/usb.mk
 
 SRCS +=	${SRC}/lib/usb/usb_common.c \
 	${SRC}/lib/usb/usb_cdc.c \
+
+SRCS-${ENABLE_NET_MBUS} += \
 	${SRC}/lib/usb/usb_mbus.c \
 
 ${MO}/src/lib/usb/%.o : CFLAGS += ${NOFPU}
