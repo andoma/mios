fba627a5dbc038ddf92eea0712dce8662b4333be
Andreas Smas <andreas@lonelycoder.com>
2021-10-15 17:56:18 +0200

    assert(): Add __builtin_expect()

--- include/assert.h
+++ include/assert.h
@@ -4,7 +4,7 @@ void __assert_func(const char *expr, const char *file, int line)
   __attribute__((noreturn));
 
 #ifndef NDEBUG
-#define assert(EX) (void)((EX) || (__assert_func (#EX, __FILE__, __LINE__),0))
+#define assert(EX) (void)((__builtin_expect(EX, 1)) || (__assert_func (#EX, __FILE__, __LINE__),0))
 #else
 #define assert(EX)
 #endif
