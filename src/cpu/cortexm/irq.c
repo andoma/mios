5285f22bc2fd7827a15121e4a972e6c84b2ea521
Andreas Smas <andreas@lonelycoder.com>
2021-10-23 17:38:37 +0200

    cortexm: Add irq_enable_fn_arg()

--- src/cpu/cortexm/irq.c
+++ src/cpu/cortexm/irq.c
@@ -1,5 +1,8 @@
+#include <assert.h>
 #include <stdint.h>
-
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
 #include <mios/mios.h>
 #include "irq.h"
 
@@ -17,6 +20,10 @@ irq(void)
 static volatile unsigned int * const NVIC_ISER = (unsigned int *)0xe000e100;
 static volatile unsigned int * const NVIC_ICER = (unsigned int *)0xe000e180;
 static volatile uint8_t * const NVIC_IPR  = (uint8_t *)0xe000e400;
+static volatile unsigned int * const SYST_SHPR3 = (unsigned int *)0xe000ed20;
+static volatile unsigned int * const VTOR  = (unsigned int *)0xe000ed08;
+
+extern uint32_t vectors[];
 
 void
 irq_enable(int irq, int level)
@@ -32,14 +39,56 @@ irq_disable(int irq)
 }
 
 
-static volatile unsigned int * const SYST_SHPR3 = (unsigned int *)0xe000ed20;
-static volatile unsigned int * const VTOR  = (unsigned int *)0xe000ed08;
+void
+irq_enable_fn(int irq, int level, void (*fn)(void))
+{
+  assert(irq < CORTEXM_IRQ_COUNT);
+  int q = irq_forbid(IRQ_LEVEL_ALL);
 
+  uint32_t curvecs = *VTOR;
 
-static void __attribute__((constructor(140)))
+  if(curvecs == (uint32_t)&vectors) {
+    // Not yet relocated
+    const size_t vecsize = (16 + CORTEXM_IRQ_COUNT) * sizeof(void *);
+    void *p = xalloc(vecsize, 0x100, 0);
+    memcpy(p, &vectors, vecsize);
+    *VTOR = (uint32_t)p;
+  }
+
+  uint32_t *vtable = (uint32_t *)*VTOR;
+  vtable[irq + 16] = (uint32_t)fn;
+  NVIC_IPR[irq] = IRQ_LEVEL_TO_PRI(level);
+  NVIC_ISER[(irq >> 5) & 7] |= 1 << (irq & 0x1f);
+  irq_permit(q);
+}
+
+void
+irq_enable_fn_arg(int irq, int level, void (*fn)(void *arg), void *arg)
+{
+  uint32_t *p = malloc(16);
+
+  /*
+    ldr r3, [pc, #4]  // fn -> r3
+    nop
+    ldr r0, [pc, #4]  // arg -> r0
+    bx r3
+  */
+
+  // NOP is encoded differently on thumb and thumb2
+#ifdef __thumb2__
+  p[0] = 0xbf004b01;
+#else
+  p[0] = 0x46c04b01;
+#endif
+  p[1] = 0x47184801;
+  p[2] = (uint32_t)fn;
+  p[3] = (uint32_t)arg;
+  irq_enable_fn(irq, level, (void *)p + 1);
+}
+
+static void __attribute__((constructor(101)))
 irq_init(void)
 {
-  extern int *vectors;
   *VTOR = (uint32_t)&vectors;
 
   *SYST_SHPR3 =
