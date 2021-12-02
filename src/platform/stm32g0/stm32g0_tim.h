d3c999fb58f801d5f1f1a5694a518e54d14174c1
Andreas Smas <andreas@lonelycoder.com>
2021-12-02 17:34:23 +0100

    stm32g0_tim: Add more register defines

--- src/platform/stm32g0/stm32g0_tim.h
+++ src/platform/stm32g0/stm32g0_tim.h
@@ -18,7 +18,16 @@
 #define TIMx_DIER  0x0c
 #define TIMx_SR    0x10
 #define TIMx_EGR   0x14
+#define TIMx_CCMR1 0x18
+#define TIMx_CCMR2 0x1c
+#define TIMx_CCER  0x20
 
 #define TIMx_CNT   0x24
 #define TIMx_PSC   0x28
 #define TIMx_ARR   0x2c
+
+#define TIMx_CCR1  0x34
+#define TIMx_CCR2  0x38
+#define TIMx_CCR3  0x3c
+#define TIMx_CCR4  0x40
+#define TIMx_BDTR  0x44
