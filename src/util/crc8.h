6a62e67c1f447041a8dfeebd7e0df38bb86f157c
Andreas Smas <andreas@lonelycoder.com>
2021-12-04 09:49:49 +0100

    Add CRC8

---
+++ src/util/crc8.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+uint8_t crc8(uint8_t in, const void *data, size_t len);
--- src/util/util.mk
+++ src/util/util.mk
@@ -1,6 +1,7 @@
 GLOBALDEPS += ${SRC}/util/util.mk
 
 SRCS += ${SRC}/util/crc32.c \
+	${SRC}/util/crc8.c \
 	${SRC}/util/hdlc.c \
 	${SRC}/util/pkv.c \
 	${SRC}/util/dsig.c \
