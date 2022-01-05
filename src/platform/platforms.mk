c6f02b755bb77c179c5700ffe2f81edd8f75edbe
Andreas Smas <andreas@lonelycoder.com>
2022-01-05 17:14:29 +0100

    platforms: Add stm32wb55-nucleo64 to allplatforms target

--- src/platform/platforms.mk
+++ src/platform/platforms.mk
@@ -1,10 +1,10 @@
-
 ALLPLATFORMS := \
 	lm3s811evb \
 	stm32f405-feather \
 	stm32g0-nucleo64 \
 	stm32f407g-disc1 \
 	stm32h7-nucleo144 \
+	stm32wb55-nucleo64 \
 	bluefruit-nrf52 \
 
 ${ALLPLATFORMS}:
