52fdf42fb481b296913a9ba0f2e16ba969be0c8c
Andreas Smas <andreas@lonelycoder.com>
2022-01-05 17:09:38 +0100

    stm32: Create a shared io_types.h

---
+++ src/platform/stm32/stm32_io_types.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// Included by io.h
+
+#define GPIO(PORT, BIT) (((PORT) << 4) | (BIT))
+
+#define GPIO_PA(x)  GPIO(0, x)
+#define GPIO_PB(x)  GPIO(1, x)
+#define GPIO_PC(x)  GPIO(2, x)
+#define GPIO_PD(x)  GPIO(3, x)
+#define GPIO_PE(x)  GPIO(4, x)
+#define GPIO_PF(x)  GPIO(5, x)
+#define GPIO_PG(x)  GPIO(6, x)
+#define GPIO_PH(x)  GPIO(7, x)
+#define GPIO_PI(x)  GPIO(8, x)
+#define GPIO_PJ(x)  GPIO(9, x)
+#define GPIO_PK(x)  GPIO(10, x)
+
+#define GPIO_UNUSED 0xff
+
+typedef unsigned char gpio_t;
+
+void gpio_conf_af(gpio_t gpio, int af, gpio_output_type_t type,
+                  gpio_output_speed_t speed, gpio_pull_t pull);
+
+typedef struct {
+  gpio_t gpio;
+  uint8_t af;
+} gpio_af_t;
