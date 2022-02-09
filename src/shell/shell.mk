b9afd73e999caea249fa748dac5426114146c242
Andreas Smas <andreas@lonelycoder.com>
2022-02-09 08:24:13 +0100

    Remove msmp (replaced by mbus)

--- src/shell/shell.mk
+++ src/shell/shell.mk
@@ -1,6 +1,5 @@
 SRCS +=	${SRC}/shell/cli.c \
 	${SRC}/shell/monitor.c \
-	${SRC}/shell/msmp.c \
 	${SRC}/shell/cmd_i2c.c \
 
 ${MOS}/shell/%.o : CFLAGS += ${NOFPU}
--- support/msmp/mios/stream.h
+++
@@ -1,1 +0,0 @@
-../../../include/mios/stream.h
