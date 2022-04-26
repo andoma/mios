4e998447a2b400305ab146b7c958be9a27130f02
Andreas Smas <andreas@lonelycoder.com>
2022-04-26 17:01:10 +0200

    stm32g0: Add TIMx_CCRx()

--- src/platform/stm32g0/stm32g0_tim.h
+++ src/platform/stm32g0/stm32g0_tim.h
@@ -26,10 +26,14 @@
 #define TIMx_PSC   0x28
 #define TIMx_ARR   0x2c
 
-#define TIMx_CCR1  0x34
-#define TIMx_CCR2  0x38
-#define TIMx_CCR3  0x3c
-#define TIMx_CCR4  0x40
+#define TIMx_CCRx(x) (0x30 + (x) * 4)
+
+#define TIMx_CCR1  TIMx_CCRx(1)
+#define TIMx_CCR2  TIMx_CCRx(2)
+#define TIMx_CCR3  TIMx_CCRx(3)
+#define TIMx_CCR4  TIMx_CCRx(4)
+
+
 #define TIMx_BDTR  0x44
 #define TIMx_CCMR3 0x54
 #define TIMx_CCR5  0x58
