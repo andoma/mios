1b097390d9f1077e7bd1c7a59fcb7b52f0102a57
Andreas Smas <andreas@lonelycoder.com>
2022-10-15 20:43:50 +0200

    Fix hrtimer for stm32g0

--- src/platform/stm32g0/stm32g0_hrtim.c
+++ src/platform/stm32g0/stm32g0_hrtim.c
@@ -1,25 +1,11 @@
-#ifdef HAVE_HRTIMER
-
 #include "stm32g0_reg.h"
 #include "stm32g0_tim.h"
 #include "stm32g0_clk.h"
-
-
-#define HRTIM_BASE TIM7_BASE
 
 #include "platform/stm32/stm32_hrtim.c"
 
 static void  __attribute__((constructor(131)))
 stm32g0_tim_init(void)
 {
-  hrtimer_init(CLK_TIM7);
-  irq_enable(18, IRQ_LEVEL_CLOCK);
+  hrtimer_init(TIM7_BASE, CLK_TIM7, 18);
 }
-
-void
-irq_18(void)
-{
-  hrtim_irq();
-}
-
-#endif
