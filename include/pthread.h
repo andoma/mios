2bf59be83463e2eb0775b2781dd396b88a6f0fbc
Andreas Smas <andreas@lonelycoder.com>
2023-01-25 14:50:56 +0100

    pthread: Add pthread_condattr_t

--- include/pthread.h
+++ include/pthread.h
@@ -17,6 +17,8 @@ typedef mutex_t pthread_mutex_t;
 
 typedef cond_t pthread_cond_t;
 
+typedef void pthread_condattr_t;
+
 #define pthread_cond_init(c, a) cond_init(c, NULL)
 #define pthread_cond_signal(c)  cond_signal(c)
 #define pthread_cond_wait(c, m) cond_wait(c, m)
