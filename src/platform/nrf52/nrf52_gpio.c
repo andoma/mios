5e31a282d39348ca6dbfefbb7e78c215a8ae03ed
Andreas Smas <andreas@lonelycoder.com>
2023-02-13 19:39:53 +0100

    nrf52: Add missing GPIO functionality

--- src/platform/nrf52/nrf52_gpio.c
+++ src/platform/nrf52/nrf52_gpio.c
@@ -2,12 +2,17 @@
 
 #include "nrf52_reg.h"
 
+#include "irq.h"
+
+#include <stdio.h>
+
 #define GPIO_BASE 0x50000000
 
 #define GPIO_PIN_CNF(x) (GPIO_BASE + 0x700 + (x) * 4)
 
 #define GPIO_OUTSET (GPIO_BASE + 0x508)
 #define GPIO_OUTCLR (GPIO_BASE + 0x50c)
+#define GPIO_IN     (GPIO_BASE + 0x510)
 
 static const uint8_t pullmap[3] = {0, 3, 1};
 
@@ -28,10 +33,83 @@ gpio_conf_output(gpio_t gpio, gpio_output_type_t type,
 
 
 void
+gpio_conf_input(gpio_t gpio, gpio_pull_t pull)
+{
+  const uint32_t reg =
+    (0 << 0) |                        // INPUT
+    (0 << 1) |                        // Connect INPUT (0 = yes)
+    (pullmap[pull]  << 2);
+  reg_wr(GPIO_PIN_CNF(gpio), reg);
+}
+
+
+int
+gpio_get_input(gpio_t gpio)
+{
+  return (reg_rd(GPIO_IN) >> gpio) & 1;
+}
+
+void
 gpio_set_output(gpio_t gpio, int on)
 {
-  if(on)
-    reg_wr(GPIO_OUTSET, 1 << gpio);
-  else
-    reg_wr(GPIO_OUTCLR, 1 << gpio);
+  reg_wr(on ? GPIO_OUTSET : GPIO_OUTCLR, 1 << gpio);
+}
+
+
+
+typedef struct {
+  void (*cb)(void *arg);
+  void *arg;
+} gpiote_event_t;
+
+static gpiote_event_t gpiote_events[8];
+
+#define GPIOTE_BASE       0x40006000
+
+#define GPIOTE_EVENTS_IN(x) (GPIOTE_BASE + 0x100 + (x) * 4)
+#define GPIOTE_CONFIG(x)    (GPIOTE_BASE + 0x510 + (x) * 4)
+#define GPIOTE_INTENSET     (GPIOTE_BASE + 0x304)
+#define GPIOTE_INTENCLR     (GPIOTE_BASE + 0x308)
+
+// GPIOTE
+void
+irq_6(void)
+{
+  for(int i = 0; i < 8; i++) {
+    if(reg_rd(GPIOTE_EVENTS_IN(i))) {
+      reg_wr(GPIOTE_EVENTS_IN(i), 0);
+      gpiote_events[i].cb(gpiote_events[i].arg);
+    }
+  }
+}
+
+
+void
+gpio_conf_irq(gpio_t gpio, gpio_pull_t pull,
+              void (*cb)(void *arg), void *arg,
+              gpio_edge_t edge, int level)
+{
+
+  irq_enable(6, level); // Increase loopcount to 8 once we fix IRQ level mixing
+  gpio_conf_input(gpio, pull);
+
+  for(int i = 0; i < 1; i++) {
+    if(gpiote_events[i].cb)
+      continue;
+
+    uint32_t config = 1; // MODE=EVENT
+
+    config |= gpio << 8;
+
+    if(edge & GPIO_FALLING_EDGE)
+      config |= (1 << 17);
+    if(edge & GPIO_RISING_EDGE)
+      config |= (1 << 16);
+    reg_wr(GPIOTE_CONFIG(i), config);
+    gpiote_events[i].arg = arg;
+    gpiote_events[i].cb = cb;
+    reg_wr(GPIOTE_INTENSET, (1 << i));
+    return;
+  }
+  panic("Out of GPIOTE event slots");
 }
