2b9277be477255db436affe0343a38058c39c81e
Andreas Smas <andreas@lonelycoder.com>
2023-08-10 08:29:49 +0200

    lfs: Add all relevant defines in a header file

--- src/lib/littlefs.mk
+++ src/lib/littlefs.mk
@@ -7,7 +7,7 @@ SRCS-${ENABLE_LITTLEFS} += \
 	${SRC}/lib/littlefs/lfs_util.c \
 	${SRC}/lib/fs.c \
 
-LITTLEFS_CFLAGS += -DLFS_NO_ASSERT -DLFS_THREADSAFE
+LITTLEFS_CFLAGS += -include ${SRC}/lib/littlefs_mios.h
 
 ${MOS}/lib/littlefs/%.o : CFLAGS += \
 	-Wframe-larger-than=192 ${LITTLEFS_CFLAGS} ${NOFPU}
---
+++ src/lib/littlefs_mios.h
@@ -0,0 +1,11 @@
+#define LFS_NO_ASSERT
+
+#define LFS_THREADSAFE
+
+#include <mios/eventlog.h>
+
+#define LFS_NO_DEBUG
+
+#define LFS_WARN(fmt...) evlog0(LOG_WARNING, NULL, fmt)
+
+#define LFS_ERROR(fmt...) evlog0(LOG_ERR, NULL, fmt)
