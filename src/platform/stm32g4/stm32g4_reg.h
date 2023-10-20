1b6c8c528b0f8cbdb4d0526b0bc3da6fd1c16a2c
Andreas Smas <andreas@lonelycoder.com>
2023-10-20 17:15:30 +0200

    stm32: Make reg_{set,clr}_bit use bit-banding if possible at comptime

--- src/platform/stm32g4/stm32g4_reg.h
+++ src/platform/stm32g4/stm32g4_reg.h
@@ -54,17 +54,31 @@ reg_set_bits(uint32_t addr, uint32_t shift, uint32_t length, uint32_t bits)
   *ptr = (*ptr & ~mask) | ((bits << shift) & mask);
 }
 
-static inline void
-reg_set_bit(uint32_t addr, uint32_t bit)
+static inline uint32_t
+pr_bb(uint32_t addr, int bit)
 {
-  reg_set_bits(addr, bit, 1, 1);
+  return ((bit * 4) + (addr << 5)) | 0x02000000 | (addr & 0xf0000000);
 }
 
-static inline void
-reg_clr_bit(uint32_t addr, uint32_t bit)
-{
-  reg_set_bits(addr, bit, 1, 0);
-}
+#define reg_set_bit(addr, bit) do {                             \
+    if(__builtin_constant_p(addr) &&                            \
+       (addr) >= 0x40000000 && (addr) < 0x41000000) {           \
+      volatile uint32_t *ptr = (uint32_t *)pr_bb(addr, bit);    \
+      *ptr = 1;                                                 \
+    } else {                                                    \
+      reg_set_bits(addr, bit, 1, 1);                            \
+    }                                                           \
+  } while(0)
+
+#define reg_clr_bit(addr, bit) do {                             \
+    if(__builtin_constant_p(addr) &&                            \
+     (addr) >= 0x40000000 && (addr) < 0x41000000) {             \
+    volatile uint32_t *ptr = (uint32_t *)pr_bb(addr, bit);      \
+    *ptr = 0;                                                   \
+  } else {                                                      \
+    reg_set_bits(addr, bit, 1, 0);                              \
+  }                                                             \
+} while(0)
 
 
 static inline int
