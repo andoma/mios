aca7f584e19d6de0bac5b2ee58664f3adeb5f326
Andreas Smas <andreas@lonelycoder.com>
2023-10-25 13:14:32 +0200

    Remove unused function declarations

--- include/unistd.h
+++ include/unistd.h
@@ -4,13 +4,9 @@
 
 void usleep(unsigned int delta);
 
-void usleep_hr(unsigned int useconds);
-
 void sleep(unsigned int seconds);
 
 void sleep_until(uint64_t deadline);
-
-void sleep_until_hr(uint64_t deadline);
 
 uint64_t clock_get(void);
 
