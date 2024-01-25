4b1fcdccc57841333cb2537085a5966085e73573
Andreas Smas <andreas@lonelycoder.com>
2024-01-25 18:27:18 +0100

    Drop sx1280

--- src/drivers/drivers.mk
+++ src/drivers/drivers.mk
@@ -10,14 +10,6 @@ SRCS-${ENABLE_MPU9250} += \
 SRCS-${ENABLE_BMI120} += \
 	${SRC}/drivers/bmi120.c
 
-SRCS-${ENABLE_SX1280} += \
-	${SRC}/drivers/sx1280/sx1280.c \
-	${SRC}/drivers/sx1280/sx1280_tdma.c \
-	${SRC}/drivers/sx1280_mios.c
-
-${MOS}/drivers/sx1280/%.o : CFLAGS += ${NOFPU}
-${MOS}/drivers/sx1280_mios.o : CFLAGS += ${NOFPU}
-
 SRCS-${ENABLE_HDC1080} += \
 	${SRC}/drivers/hdc1080.c
 
