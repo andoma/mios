383ffdd6ba5a4141d8eb89596f47b70834d8420b
Andreas Smas <andreas@lonelycoder.com>
2024-01-31 08:46:37 +0100

    net/dhcp: Remove unused DHCPV4UPDATE()

--- src/net/ipv4/dhcpv4.h
+++ src/net/ipv4/dhcpv4.h
@@ -10,8 +10,3 @@ struct stream;
 void dhcpv4_status_change(struct ether_netif *eni);
 
 void dhcpv4_print(struct ether_netif *eni, struct stream *st);
-
-typedef int(*dhcpv4_update_t)(struct netif *ni);
-
-#define DHCPV4UPDATE(cb)                                                  \
-  static const dhcpv4_update_t MIOS_JOIN(dhcpv4update, __LINE__) __attribute__ ((used, section("dhcpv4update"))) = cb;
