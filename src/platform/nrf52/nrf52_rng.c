cd1d82da00dfdf15b82cd3f661338d1ad7a9ac8f
Andreas Smas <andreas@lonelycoder.com>
2024-05-17 22:41:38 +0200

    nrf52: Use HW RNG just for seeding PRNG
    This saves power

--- src/platform/nrf52/nrf52_rng.c
+++ src/platform/nrf52/nrf52_rng.c
@@ -7,17 +7,27 @@
 #include "nrf52_rng.h"
 #include "nrf52_rtc.h"
 
+static prng_t state;
+
 static void  __attribute__((constructor(120)))
 nrf52_rng_init(void)
 {
+  // Use hardware RNG for initial seed
   reg_wr(RNG_TASKS_START, 1);
+  reg_wr(RNG_CONFIG, 1); // Enable BIAS correction
+
+  for(int i = 0; i < 8; i++) {
+    while(reg_rd(RNG_EVENTS_VALRDY) == 0) {
+    }
+    prng_get(&state, reg_rd(RNG_VALUE));
+  }
+  reg_wr(RNG_TASKS_STOP, 1);
 }
 
 
 int
 rand(void)
 {
-  static prng_t state;
-  uint32_t seed = (reg_rd(RNG_VALUE) << 24) | reg_rd(RTC2_BASE + RTC_COUNTER);
+  uint32_t seed = reg_rd(RTC2_BASE + RTC_COUNTER);
   return prng_get(&state, seed) & RAND_MAX;
 }
--- src/platform/nrf52/nrf52_rng.h
+++ src/platform/nrf52/nrf52_rng.h
@@ -1,5 +1,8 @@
 #pragma once
 
 #define RNG_BASE         0x4000d000
-#define RNG_TASKS_START (RNG_BASE + 0x000)
-#define RNG_VALUE       (RNG_BASE + 0x508)
+#define RNG_TASKS_START    (RNG_BASE + 0x000)
+#define RNG_TASKS_STOP     (RNG_BASE + 0x004)
+#define RNG_EVENTS_VALRDY  (RNG_BASE + 0x100)
+#define RNG_CONFIG         (RNG_BASE + 0x504)
+#define RNG_VALUE          (RNG_BASE + 0x508)
