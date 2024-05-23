df478664b22e57b9b0a2c386807765ce42f860c3
Andreas Smas <andreas@lonelycoder.com>
2024-05-23 19:29:12 +0200

    stm32g0: Add built-in bootloader (which can reflash from SPI)

--- src/platform/stm32g0/stm32g0.mk
+++ src/platform/stm32g0/stm32g0.mk
@@ -4,7 +4,9 @@ GLOBALDEPS += ${P}/stm32g0.mk
 
 CPPFLAGS += -iquote${P} -include ${P}/stm32g0.h
 
-LDSCRIPT = ${P}/stm32g0$(if $(subst no,,${ENABLE_OTA}),_ota,).ld
+LDSCRIPT ?= ${P}/stm32g0$(if $(subst no,,${ENABLE_BUILTIN_BOOTLOADER}),_bootloader,).ld
+
+ENTRYPOINT ?= $(if $(subst no,,${ENABLE_BUILTIN_BOOTLOADER}),bl_start,start)
 
 include ${SRC}/cpu/cortexm/cortexm0plus.mk
 
@@ -22,16 +24,13 @@ SRCS += ${C}/systick.c \
 	${P}/stm32g0_dma.c \
 	${P}/stm32g0_flash.c \
 
-SRCS-${ENABLE_OTA} += \
-	${P}/stm32g0_ota.c \
-	${P}/boot/isr.s \
-	${P}/boot/bootloader.c \
-
 SRCS-${ENABLE_NET_MBUS} += \
 	${P}/stm32g0_uart_mbus.c
 
 SRCS-${ENABLE_SYSTIM} += ${P}/stm32g0_systim.c
 
-${MOS}/platform/stm32g0/boot/bootloader.o : CFLAGS = -Os -ffreestanding -Wall -Werror -mcpu=cortex-m0plus -mthumb -include ${BOOTLOADER_DEFS}
+SRCS-${ENABLE_BUILTIN_BOOTLOADER} += \
+	${P}/boot/stm32g0_bootloader.c \
+	${P}/boot/isr.s \
 
-${P}/boot/bootloader.c : ${O}/version_git.h
+${MOS}/platform/stm32g0/boot/stm32g0_bootloader.o : CFLAGS = -Os -ffreestanding -Wall -Werror -mcpu=cortex-m0plus -mthumb -include ${BOOTLOADER_DEFS}
