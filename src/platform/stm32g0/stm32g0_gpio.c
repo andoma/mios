512a6b4abadd6c78f8db2bc9956e27d8ee0da646
Magnus Röös <mla@cur8.co>
2024-05-27 09:17:13 +0200

    fallout on Add pull-up/dn args

--- src/platform/stm32g0/stm32g0_gpio.c
+++ src/platform/stm32g0/stm32g0_gpio.c
@@ -23,14 +23,14 @@
 
 
 void
-gpio_conf_analog(gpio_t gpio)
+gpio_conf_analog(gpio_t gpio, gpio_pull_t pull)
 {
   const int port = gpio >> 4;
   const int bit = gpio & 0xf;
   clk_enable(CLK_GPIO(port));
   int s = irq_forbid(IRQ_LEVEL_IO);
   reg_set_bits(GPIO_MODER(port), bit * 2, 2, 3);
-  reg_set_bits(GPIO_PUPDR(port), bit * 2, 2, 0);
+  reg_set_bits(GPIO_PUPDR(port), bit * 2, 2, pull);
   irq_permit(s);
 }
 
