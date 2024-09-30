906089bafe1534d2d508fa8ee8244cc054b0f42d
Magnus Röös <mla@cur8.co>
2024-09-30 08:23:23 +0000

    stm32f4: fix fallout, forgot to update Makefile from previous commit

--- src/platform/stm32f4/stm32f4.mk
+++ src/platform/stm32f4/stm32f4.mk
@@ -14,7 +14,6 @@ SRCS += ${C}/systick.c \
 	${P}/stm32f4.c \
 	${P}/stm32f4_info.c \
 	${P}/stm32f4_gpio.c \
-	${P}/stm32f4_iwdog.c \
 	${P}/stm32f4_i2c.c \
 	${P}/stm32f4_spi.c \
 	${P}/stm32f4_dma.c \
