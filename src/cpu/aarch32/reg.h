03a264c3981c12eb646b9b4c90cc776af9eab4a1
Andreas Smas <andreas@lonelycoder.com>
2025-01-11 18:25:51 +0100

    aarch32: Add bit register bit manipulation macros

--- src/cpu/aarch32/reg.h
+++ src/cpu/aarch32/reg.h
@@ -43,3 +43,25 @@ reg_rd8(uint32_t addr)
   volatile uint8_t *ptr = (uint8_t *)addr;
   return *ptr;
 }
+
+static inline void
+reg_set_bits(uint32_t addr, uint32_t shift, uint32_t length, uint32_t bits)
+{
+  volatile uint32_t *ptr = (uint32_t *)addr;
+
+  const uint32_t mask = ((1 << length) - 1) << shift;
+
+  *ptr = (*ptr & ~mask) | ((bits << shift) & mask);
+}
+
+static inline void
+reg_set_bit(uint32_t addr, uint32_t bit)
+{
+  reg_set_bits(addr, bit, 1, 1);
+}
+
+static inline void
+reg_clr_bit(uint32_t addr, uint32_t bit)
+{
+  reg_set_bits(addr, bit, 1, 0);
+}
