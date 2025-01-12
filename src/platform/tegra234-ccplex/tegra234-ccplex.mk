819f168fbe22fc8c2ae54075c8df25d059d20431
Andreas Smas <andreas@lonelycoder.com>
2025-01-12 17:11:05 +0100

    tegra234/ccplex: Cleanup startup code a bit

--- src/platform/tegra234-ccplex/tegra234-ccplex.mk
+++ src/platform/tegra234-ccplex/tegra234-ccplex.mk
@@ -4,21 +4,13 @@ GLOBALDEPS += ${P}/tegra234-ccplex.mk
 
 CFLAGS += -mcpu=cortex-a78ae
 
-CPPFLAGS += -iquote${P} -include ${P}/tegra234-ccplex.h
+CPPFLAGS += -iquote${P} -I${SRC}/platform/tegra234 -include ${P}/tegra234-ccplex.h
 
 LDSCRIPT = ${P}/tegra234-ccplex.ld
 
 include ${SRC}/cpu/aarch64/aarch64.mk
 
-SRCS += ${P}/tegra234-ccplex.c
+SRCS += ${P}/tegra234-ccplex.c \
+	${P}/tegra234-ccplex_hsp.c \
 
 ENABLE_TASK_ACCOUNTING := no
-
-GDB_PORT ?= 1234
-GDB_HOST ?= 127.0.0.1
-
-qemu: ${O}/build.elf
-	qemu-system-aarch64 -M virt,gic-version=3 -cpu cortex-a57 -nographic -kernel $< -s -S
-
-gdb: ${O}/build.elf
-	${GDB} -ex "target extended-remote ${GDB_HOST}:${GDB_PORT}" -x ${T}/gdb/macros $<
