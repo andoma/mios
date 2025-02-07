c47844573f1b3ace67d48b6ff0b0df701b127c80
Andreas Smas <andreas@lonelycoder.com>
2025-02-07 07:17:24 +0100

    aarch32: Add prefetch abort handler

--- src/cpu/aarch32/exc.c
+++ src/cpu/aarch32/exc.c
@@ -16,3 +16,16 @@ data_abort(uint32_t pc, uint32_t psr, uint32_t *regs)
 
   panic("Data abort");
 }
+
+
+void
+prefetch_abort(uint32_t pc, uint32_t psr, uint32_t *regs)
+{
+  printf("\n*** Exception: Prefetch Abort\n");
+  for(int i = 0; i < 13; i++) {
+    printf("R%-2d 0x%08x\n", i, regs[i]);
+  }
+  printf("PC  0x%08x\nPSR 0x%08x\n", pc, psr);
+
+  panic("Prefetch abort");
+}
