3f8b68c38bfa305bcd2a4f892b1922732070c844
Andreas Smas <andreas@lonelycoder.com>
2025-05-13 08:28:19 +0200

    aarch64-vir: Add 'run' as target (starts mios in qemu without gdb)

--- src/platform/aarch64-virt/aarch64-virt.mk
+++ src/platform/aarch64-virt/aarch64-virt.mk
@@ -21,5 +21,8 @@ GDB_HOST ?= 127.0.0.1
 qemu: ${O}/build.elf
 	qemu-system-aarch64 -M virt,gic-version=3 -cpu cortex-a57 -nographic -kernel $< -s -S
 
+run: ${O}/build.elf
+	qemu-system-aarch64 -m 8192 -M virt,gic-version=3 -cpu cortex-a57 -nographic -kernel $<
+
 gdb: ${O}/build.elf
 	${GDB} -ex "target extended-remote ${GDB_HOST}:${GDB_PORT}" -ex "layout asm" -ex "layout regs" -x ${T}/gdb/macros $<
