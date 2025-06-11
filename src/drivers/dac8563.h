8ce1f84421411335f12c052988d98c0e83544378
Magnus Röös <mla@nekohealth.com>
2025-06-11 10:37:32 +0200

    stm32h7_uart: make it possible to configure open drain for tx

--- src/drivers/dac8563.h
+++ src/drivers/dac8563.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdbool.h>
+
 #include <mios/io.h>
 #include <mios/imu.h>
 
@@ -9,9 +11,7 @@ typedef struct dac8563 dac8563_t;
 
 dac8563_t *dac8563_create(spi_t *bus, gpio_t nss);
 
-error_t dac8563_power_down(dac8563_t *dac);
-
-error_t dac8563_power_up(dac8563_t *dac, uint8_t channel);
+error_t dac8563_power(dac8563_t *dac, uint8_t channel, bool on);
 
 error_t dac8563_set_dac(dac8563_t *dac, uint8_t channel, uint16_t value);
 
