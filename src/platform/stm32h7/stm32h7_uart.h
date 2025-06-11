336c4646549ef476b5ee89120daa05da084a8b49
Magnus Röös <mla@nekohealth.com>
2025-06-11 10:37:32 +0200

    stm32h7_uart: make it possible to configure open drain for tx

--- src/platform/stm32h7/stm32h7_uart.h
+++ src/platform/stm32h7/stm32h7_uart.h
@@ -3,8 +3,10 @@
 
 #include "platform/stm32/stm32_uart_stream.h"
 
+#define UART_TX_OPEN_DRAIN 0x100
+
 stream_t *stm32h7_uart_init(stm32_uart_stream_t *u, unsigned int instance,
                             int baudrate,
-                            gpio_t tx, gpio_t rx, uint8_t flags,
+                            gpio_t tx, gpio_t rx, uint16_t flags,
                             const char *name);
 
