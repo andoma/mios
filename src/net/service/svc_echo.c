1ef50d1310111f0bbfad79c6a142598015a279a6
Andreas Smas <andreas@lonelycoder.com>
2025-08-06 18:16:50 +0000

    svc/echo: Add pushpull interface

--- src/net/service/svc_echo.c
+++ src/net/service/svc_echo.c
@@ -3,6 +3,11 @@
 #include <mios/task.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+#include "net/pbuf.h"
 
 __attribute__((noreturn))
 static void *
@@ -30,10 +35,85 @@ echo_thread(void *arg)
 static error_t
 echo_open_stream(stream_t *s)
 {
-  thread_t *t = thread_create(echo_thread, s, 1024, "echo", TASK_DETACHED, 5);
+  thread_t *t = thread_create(echo_thread, s, 0, "echo", TASK_DETACHED, 5);
   if(t)
     return 0;
   return ERR_NO_MEMORY;
 }
 
-SERVICE_DEF_STREAM("echo", 7, echo_open_stream);
+
+
+
+
+typedef struct svc_echo {
+  pbuf_t *se_pb;
+  pushpull_t *se_sock;
+} svc_echo_t;
+
+static uint32_t
+echo_push(void *opaque, struct pbuf *pb)
+{
+  svc_echo_t *se = opaque;
+  assert(se->se_pb == NULL);
+  se->se_pb = pb;
+  return 0;
+}
+
+static int
+echo_may_push(void *opaque)
+{
+  svc_echo_t *se = opaque;
+  return se->se_pb == NULL;
+}
+
+
+static pbuf_t *
+echo_pull(void *opaque)
+{
+  svc_echo_t *se = opaque;
+  pushpull_t *s = se->se_sock;
+  pbuf_t *pb = se->se_pb;
+  se->se_pb = NULL;
+  s->net->event(s->net_opaque, PUSHPULL_EVENT_PUSH);
+  return pb;
+}
+
+
+static void
+echo_close(void *opaque, const char *reason)
+{
+  svc_echo_t *se = opaque;
+  pushpull_t *s = se->se_sock;
+  s->net->event(s->net_opaque, PUSHPULL_EVENT_CLOSE);
+  pbuf_free(se->se_pb);
+  free(se);
+}
+
+static const pushpull_app_fn_t echo_fn = {
+  .push = echo_push,
+  .may_push = echo_may_push,
+  .pull = echo_pull,
+  .close = echo_close
+};
+
+static error_t
+echo_open_pushpull(pushpull_t *s)
+{
+  svc_echo_t *se = xalloc(sizeof(svc_echo_t), 0, MEM_MAY_FAIL);
+  if(se == NULL)
+    return ERR_NO_MEMORY;
+
+  se->se_pb = NULL;
+  se->se_sock = s;
+  s->app = &echo_fn;
+  s->app_opaque = se;
+  return 0;
+}
+
+static const service_t svc_echo
+__attribute__ ((used, section("servicedef"))) = {
+  .name = "echo",
+  .ip_port = 7,
+  .open_stream = echo_open_stream,
+  .open_pushpull = echo_open_pushpull,
+};
