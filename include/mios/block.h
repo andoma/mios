b416fc20d307587e32e440ae83fa571118325669
Andreas Smas <andreas@lonelycoder.com>
2025-09-19 15:28:28 +0200

    block: Add helper functions for method invokation

--- include/mios/block.h
+++ include/mios/block.h
@@ -41,3 +41,34 @@ block_iface_t *block_create_partition(block_iface_t *parent,
 #define BLOCK_VERIFIER_DUMP         0x2
 
 block_iface_t *block_create_verifier(block_iface_t *parent, int flags);
+
+
+__attribute__((always_inline))
+static inline error_t
+block_erase(block_iface_t *bi, size_t block, size_t count)
+{
+  return bi->erase(bi, block, count);
+}
+
+__attribute__((always_inline))
+static inline error_t
+block_write(struct block_iface *bi, size_t block,
+            size_t offset, const void *data, size_t length)
+{
+  return bi->write(bi, block, offset, data, length);
+}
+
+__attribute__((always_inline))
+static inline error_t
+block_read(struct block_iface *bi, size_t block,
+           size_t offset, void *data, size_t length)
+{
+  return bi->read(bi, block, offset, data, length);
+}
+
+__attribute__((always_inline))
+static inline error_t
+block_ctrl(struct block_iface *bi, block_ctrl_op_t op)
+{
+  return bi->ctrl(bi, op);
+}
