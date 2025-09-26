fc186bfb641f02858c06d0b6c23679935c727a43
Andreas Smas <andreas@lonelycoder.com>
2025-09-26 15:38:01 +0200

    mios_image: Add support for 64bit ELF

--- host/mios_image.c
+++ host/mios_image.c
@@ -55,9 +55,56 @@ typedef struct elf32shdr {
   uint32_t entsize;
 } elf32shdr_t;
 
+typedef struct elf64hdr {
+  uint32_t magic;
+  uint8_t bits;
+  uint8_t endian;
+  uint8_t header_version;
+  uint8_t abi;
+  uint8_t pad[8];
+  uint16_t type;
+  uint16_t instruction_set;
+  uint32_t elf_version;
+  uint64_t program_entry_offset;
+  uint64_t program_header_table_offset;
+  uint64_t section_header_table_offset;
+  uint32_t flags;
+  uint16_t elf_header_size;
+  uint16_t program_header_entry_size;
+  uint16_t program_header_entries;
+  uint16_t section_header_entry_size;
+  uint16_t section_header_entries;
+  uint16_t string_table_section_entry;
+} elf64hdr_t;
+
+
+typedef struct elf64phdr {
+  uint32_t type;
+  uint32_t flags;
+  uint64_t offset;
+  uint64_t vaddr;
+  uint64_t paddr;
+  uint64_t filesz;
+  uint64_t memsz;
+  uint64_t alignment;
+} elf64phdr_t;
+
+typedef struct elf64shdr {
+  uint32_t name;
+  uint32_t type;
+  uint64_t flags;
+  uint64_t addr;
+  uint64_t offset;
+  uint64_t size;
+  uint32_t link;
+  uint32_t info;
+  uint64_t align;
+  uint64_t entsize;
+} elf64shdr_t;
+
 
 static const char *
-get_shstrtab(const void *elf)
+get_shstrtab32(const void *elf)
 {
   // TODO: Check all the boundaries, this is so unsafe
 
@@ -69,14 +116,27 @@ get_shstrtab(const void *elf)
   return elf + shdr->offset;
 }
 
+static const char *
+get_shstrtab64(const void *elf)
+{
+  // TODO: Check all the boundaries, this is so unsafe
+
+  const elf64hdr_t *elfhdr = elf;
+
+  const elf64shdr_t *shdr = elf + elfhdr->section_header_table_offset +
+    elfhdr->string_table_section_entry * elfhdr->section_header_entry_size;
+
+  return elf + shdr->offset;
+}
+
 mios_image_t *
-mios_image_from_elf_mem(const void *elf, size_t elfsize,
-                        size_t skip_bytes, size_t alignment,
-                        const char **errmsg)
+mios_image_from_elf_mem32(const void *elf, size_t elfsize,
+                          size_t skip_bytes, size_t alignment,
+                          const char **errmsg)
 {
   const elf32hdr_t *elfhdr = elf;
 
-  const char *shstrtab = get_shstrtab(elf);
+  const char *shstrtab = get_shstrtab32(elf);
 
   uint32_t image_begin = UINT32_MAX;
   uint32_t image_end = 0;
@@ -162,6 +222,145 @@ mios_image_from_elf_mem(const void *elf, size_t elfsize,
   }
   return mi;
 }
+
+
+uint64_t
+vtop64(const void *elf, uint64_t addr)
+{
+  const elf64hdr_t *elfhdr = elf;
+
+  for(size_t i = 0; i < elfhdr->program_header_entries; i++) {
+    const elf64phdr_t *phdr = elf + elfhdr->program_header_table_offset +
+      i * elfhdr->program_header_entry_size;
+
+    if(phdr->filesz == 0)
+      continue;
+    if(addr >= phdr->vaddr && addr < phdr->vaddr + phdr->filesz) {
+      return addr - phdr->vaddr + phdr->paddr;
+    }
+  }
+  return 0;
+}
+
+
+
+mios_image_t *
+mios_image_from_elf_mem64(const void *elf, size_t elfsize,
+                          size_t skip_bytes, size_t alignment,
+                          const char **errmsg)
+{
+  const elf64hdr_t *elfhdr = elf;
+
+  const char *shstrtab = get_shstrtab64(elf);
+
+  uint64_t image_begin = UINT64_MAX;
+  uint64_t image_end = 0;
+
+  for(size_t i = 0; i < elfhdr->program_header_entries; i++) {
+    const elf64phdr_t *phdr = elf + elfhdr->program_header_table_offset +
+      i * elfhdr->program_header_entry_size;
+
+    if(phdr->filesz == 0)
+      continue;
+
+    image_begin = MIN(phdr->paddr, image_begin);
+    image_end = MAX(phdr->paddr + phdr->filesz, image_end);
+  }
+
+  image_begin += skip_bytes;
+
+  if(image_begin > image_end) {
+    *errmsg = "skip_bytes is larger than entire image";
+    return NULL;
+  }
+
+  for(size_t i = 0; i < elfhdr->section_header_entries; i++) {
+    const elf64shdr_t *shdr = elf + elfhdr->section_header_table_offset +
+      i * elfhdr->section_header_entry_size;
+
+    const char *nam = shstrtab + shdr->name;
+    if(!strcmp(nam, ".text") || !strcmp(nam, ".isr_vector")) {
+      if(shdr->addr < image_begin) {
+        *errmsg = "skip_bytes skips into text segment";
+        return NULL;
+      }
+    }
+  }
+
+  size_t image_size = image_end - image_begin;
+  if(alignment == 0)
+    alignment = 64;
+  image_size = ((image_size +  alignment - 1) / alignment) * alignment;
+
+  mios_image_t *mi = malloc(sizeof(mios_image_t) + image_size);
+  memset(mi, 0, sizeof(mios_image_t));
+  memset(mi->image, 0xff, image_size);
+
+  mi->load_addr = image_begin;
+  mi->image_size = image_size;
+
+  for(size_t i = 0; i < elfhdr->program_header_entries; i++) {
+    const elf64phdr_t *phdr = elf + elfhdr->program_header_table_offset +
+      i * elfhdr->program_header_entry_size;
+
+    if(image_begin > phdr->paddr)
+      continue;
+
+    if(image_begin > phdr->paddr + phdr->filesz)
+      continue;
+
+    uint64_t image_offset = phdr->paddr - image_begin;
+    memcpy(mi->image + image_offset, elf + phdr->offset, phdr->filesz);
+  }
+
+  for(size_t i = 0; i < elfhdr->section_header_entries; i++) {
+
+    const elf64shdr_t *shdr = elf + elfhdr->section_header_table_offset +
+      i * elfhdr->section_header_entry_size;
+
+    const char *nam = shstrtab + shdr->name;
+    if(!strcmp(nam, ".build_id")) {
+      memcpy(mi->buildid, elf + shdr->offset + 16, 20);
+      mi->buildid_paddr = shdr->addr + 16;
+    }
+
+    if(!strcmp(nam, ".miosversion")) {
+      memcpy(mi->mios_version, elf + shdr->offset, 21);
+    }
+
+    if(!strcmp(nam, ".appversion")) {
+      memcpy(mi->app_version, elf + shdr->offset, 21);
+    }
+
+    if(!strcmp(nam, ".appname")) {
+      uint64_t paddr = vtop64(elf, shdr->addr);
+      if(paddr != 0)
+        mi->appname = (const char *)mi->image + paddr - image_begin;
+    }
+  }
+  return mi;
+}
+
+mios_image_t *mios_image_from_elf_mem(const void *elf, size_t elfsize,
+                                      size_t skip_bytes, size_t alignment,
+                                      const char **errmsg)
+{
+  const elf32hdr_t *elfhdr = elf;
+  if(elfhdr->magic != 0x464c457f) {
+    *errmsg = "Not an ELF-file (Incorrect magic)";
+    return NULL;
+  }
+  switch(elfhdr->bits) {
+  case 1:
+    return mios_image_from_elf_mem32(elf, elfsize, skip_bytes, alignment, errmsg);
+  case 2:
+    return mios_image_from_elf_mem64(elf, elfsize, skip_bytes, alignment, errmsg);
+  default:
+    *errmsg = "Unsupported ELF file (not 32 or 64 bit)";
+    return NULL;
+  }
+}
+
 
 
 mios_image_t *
--- host/mios_image.h
+++ host/mios_image.h
@@ -8,9 +8,9 @@ typedef struct mios_image {
   uint8_t app_version[21];  // 20 bytes SHA1 + 1 byte dirty flag
   uint8_t mios_version[21]; // 20 bytes SHA1 + 1 byte dirty flag
   uint8_t buildid[20];      // 20 byte SHA1 from toolchain
-  uint32_t buildid_paddr;   // Physical address of buildid on chip
+  uint64_t buildid_paddr;   // Physical address of buildid on chip
 
-  uint32_t load_addr;       // Where to start writing image to flash
+  uint64_t load_addr;       // Where to start writing image to flash
   size_t image_size;        // Number of bytes in image[]
   uint8_t image[0];         // Actual bytes
 } mios_image_t;
