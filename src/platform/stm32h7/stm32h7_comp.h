ce3b6b81038d4a9c14e55991fe6cae6d371816a9
Andreas Smas <andreas@lonelycoder.com>
2025-12-08 17:44:46 +0100

    stm32h7: Add register defines for COMP and DAC peripherals

---
+++ src/platform/stm32h7/stm32h7_comp.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define COMP_BASE 0x58003800
+
+#define COMP_SR     0x00
+#define COMP_ICFR   0x04
+#define COMP_OR     0x08
+#define COMP_CFGR1  0x0c
+#define COMP_CFGR2  0x10
---
+++ src/platform/stm32h7/stm32h7_dac.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define DAC_BASE      0x40007400
+
+#define DAC_CR        0x00
+#define DAC_DHR12R1   0x08
+#define DAC_DHR12R2   0x14
+#define DAC_MCR       0x3c
